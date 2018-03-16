b7b5112d21b49f0fea988a86e9209442894ae786
Tianhao <zhangth1990@hotmail.com>
2018-03-15 23:52:30 -0600

    added image and seekbar

--- app/src/main/java/com/example/tianhao/timer/MainActivity.java
+++ app/src/main/java/com/example/tianhao/timer/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tianhao.timer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.laser);
+        SeekBar bar = (SeekBar)findViewById(R.id.seekBar);
+        final TextView timer = (TextView)findViewById(R.id.textView);
+
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                timer.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
-        new CountDownTimer(10000, 100){
+        new CountDownTimer(10000, 1000){
             public void onTick(long millisecondsUntilDone){
-                Log.i("Second Left!",String.valueOf(millisecondsUntilDone/100 ));
+                Log.i("Second Left!",String.valueOf(millisecondsUntilDone/1000 ));
+                //timer.setText(String.valueOf(millisecondsUntilDone/1000 ));
             }
             public void onFinish(){
                 Log.i("We are done!", "No more countdown");
+                mediaPlayer.start();
             }
         }.start();
 
